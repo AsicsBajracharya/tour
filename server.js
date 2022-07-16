ca6a53b8a54a2698e927110ee222a2f255e404d2
Ashish Bajracharya <machiavellian2333@gmail.com>
2022-07-16 17:34:59 +0545

    connecting express app to the atlas and local database

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+//FOR HOSTED DATABASE
+const DB = process.env.DATABASE.replace(
+  //CREATING THE CONNECTION STRING
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB CONNECTION SUCCESSFUL');
+  });
+
+//FOR DATABASE LOCAL
+
+// const DB = process.env.DATABASE_LOCAL.replace(
+//   //CREATING THE CONNECTION STRING
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => {
+//     console.log('DB CONNECTION SUCCESSFUL');
+//   });
 
 // console.log(app.get('env')); //SET BY EXPRESS
 // console.log(process.env); //COMES FROM THE PROCESS CORE MODULE
