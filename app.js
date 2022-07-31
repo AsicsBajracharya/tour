56f13b58766957adf432613b988c0d66f103c6b5
Ashish Bajracharya <machiavellian2333@gmail.com>
2022-07-31 11:38:22 +0545

    global error handling

--- app.js
+++ app.js
@@ -37,9 +37,24 @@ app.use('/api/v1/users', userRouter);
 // UNHANDLED ROUTES
 app.all('*', (req, res, next) => {
   //all STANDS FOR ALL THE VERBS
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+//ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
