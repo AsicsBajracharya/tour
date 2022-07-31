bd5e36cc0e814b00611282affe8d41ed096ec24a
Ashish Bajracharya <machiavellian2333@gmail.com>
2022-07-31 11:02:17 +0545

    handling unhandled routes

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(express.json()); //ADDS DATA TO THE REQUEST BODY
 app.use(express.static(`${__dirname}/public`)); //SERVES STATIC FILES
 
 //CUSTOM MIDDLEWARE
-app.use((req, res, next) => {
-  //'use' and 'next' DENOTES A MIDDLEWARE
-  console.log('HELLO FROM THE MIDDLEWARE');
-  next();
-});
+// app.use((req, res, next) => {
+//   //'use' and 'next' DENOTES A MIDDLEWARE
+//   console.log('HELLO FROM THE MIDDLEWARE');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -33,5 +33,14 @@ app.use((req, res, next) => {
 // MOUNTING ROUTER
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// UNHANDLED ROUTES
+app.all('*', (req, res, next) => {
+  //all STANDS FOR ALL THE VERBS
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
